1855b04051bba6863429557472806d4efe596f73
Yumadilov Timur <timurice@mail.ru>
2019-01-21 18:10:39 +0500

    homework-7

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -7,8 +7,165 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Обратите внимание на `orders.test.js`.
 // Он поможет понять, какие значения должен возвращать редьюсер.
 
+// export default (state = [], action) => {
+//   switch (action.type) {
+//     case CREATE_NEW_ORDER:
+//       return [
+//         ...state,
+//         {
+//           id: action.payload.id,
+//           recipe: [...action.payload.recipe]
+//         }
+//       ];
+
+//     case MOVE_ORDER_NEXT:
+//       return state.map(order => {
+//         if (order.id === action.payload) {
+//           switch (order.position) {
+//             case 'clients':
+//               return { ...order, position: 'conveyor_1' };
+//             case 'conveyor_1':
+//               return { ...order, position: 'conveyor_2' };
+//             case 'conveyor_2':
+//               return { ...order, position: 'conveyor_3' };
+//             case 'conveyor_3':
+//               return { ...order, position: 'conveyor_4' };
+//             case 'conveyor_4':
+//               const isEveryIngredientsPresent = order.recipe.every(i =>
+//                 order.ingredients.includes(i)
+//               );
+//               if (isEveryIngredientsPresent) {
+//                 return { ...order, position: 'finish' };
+//               } else {
+//                 return order;
+//               }
+//             default:
+//               return order;
+//           }
+//         } else {
+//           return order;
+//         }
+//       });
+
+// case MOVE_ORDER_BACK:
+//   return state.map(order => {
+//     if (order.id === action.payload) {
+//       switch (order.position) {
+//         case 'conveyor_4':
+//           return { ...order, position: 'conveyor_3' };
+//         case 'conveyor_3':
+//           return { ...order, position: 'conveyor_2' };
+//         case 'conveyor_2':
+//           return { ...order, position: 'conveyor_1' };
+//         default:
+//           return order;
+//       }
+//     } else {
+//       return order;
+//     }
+//   });
+
+// case ADD_INGREDIENT: {
+//   const firstOrder = state.find(
+//     order => order.position === action.payload.from
+//   );
+//   if (!firstOrder) return state;
+
+//   return state.map(order => {
+//     if (order.id === firstOrder.id) {
+//       return {
+//         ...order,
+//         ingredients: [...order.ingredients, action.payload.ingredient]
+//       };
+//     } else {
+//       return order;
+//     }
+//   });
+// }
+
+//     default:
+//       return state;
+//   }
+// };
+
 export default (state = [], action) => {
   switch (action.type) {
+    case CREATE_NEW_ORDER:
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          recipe: [...action.payload.recipe],
+          position: 'clients',
+          ingredients: []
+        }
+      ];
+
+    case MOVE_ORDER_NEXT:
+      return state.map(order => {
+        if (order.id === action.payload) {
+          switch (order.position) {
+            case 'clients':
+              return { ...order, position: 'conveyor_1' };
+            case 'conveyor_1':
+              return { ...order, position: 'conveyor_2' };
+            case 'conveyor_2':
+              return { ...order, position: 'conveyor_3' };
+            case 'conveyor_3':
+              return { ...order, position: 'conveyor_4' };
+            case 'conveyor_4':
+              const isEveryIngredientsPresent = order.recipe.every(i =>
+                order.ingredients.includes(i)
+              );
+              if (isEveryIngredientsPresent) {
+                return { ...order, position: 'finish' };
+              } else {
+                return order;
+              }
+            default:
+              return order;
+          }
+        } else {
+          return order;
+        }
+      });
+
+    case MOVE_ORDER_BACK:
+      return state.map(order => {
+        if (order.id === action.payload) {
+          switch (order.position) {
+            case 'conveyor_4':
+              return { ...order, position: 'conveyor_3' };
+            case 'conveyor_3':
+              return { ...order, position: 'conveyor_2' };
+            case 'conveyor_2':
+              return { ...order, position: 'conveyor_1' };
+            default:
+              return order;
+          }
+        } else {
+          return order;
+        }
+      });
+
+    case ADD_INGREDIENT: {
+      const firstOrder = state.find(
+        order => order.position === action.payload.from
+      );
+      if (!firstOrder) return state;
+
+      return state.map(order => {
+        if (order.id === firstOrder.id) {
+          return {
+            ...order,
+            ingredients: [...order.ingredients, action.payload.ingredient]
+          };
+        } else {
+          return order;
+        }
+      });
+    }
+
     default:
       return state;
   }
